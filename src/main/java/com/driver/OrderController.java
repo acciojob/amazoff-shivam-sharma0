7362394426e980c28f6c4dbd19b31c3e413d48ca
shivam-sharma0 <vashistshivam931@gmail.com>
2023-05-29 18:07:58 +0530

    Ninth Try

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -1,10 +1,7 @@
 package com.driver;
 
 import java.util.List;
-import java.util.Optional;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,87 +14,113 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@Getter
-@Setter
+
 @RestController
 @RequestMapping("orders")
 public class OrderController {
 
-  @Autowired
-  private OrderService orderService;
+
+    OrderService orderService = new OrderService();
+
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
+
         orderService.addOrder(order);
         return new ResponseEntity<>("New order added successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/add-partner/{partnerId}")
     public ResponseEntity<String> addPartner(@PathVariable String partnerId){
-         orderService.addPartner(partnerId);
+
+        orderService.addDeliveryPartner(partnerId);
         return new ResponseEntity<>("New delivery partner added successfully", HttpStatus.CREATED);
     }
 
     @PutMapping("/add-order-partner-pair")
     public ResponseEntity<String> addOrderPartnerPair(@RequestParam String orderId, @RequestParam String partnerId){
-          orderService.addOrderPartnerPair(orderId,partnerId);
+
         //This is basically assigning that order to that partnerId
+        orderService.addOrderPartnerPair(orderId,partnerId);
         return new ResponseEntity<>("New order-partner pair added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/get-order-by-id/{orderId}")
     public ResponseEntity<Order> getOrderById(@PathVariable String orderId){
-        Order order= order=orderService.getOrderById(orderId);
+
+        Order order= null;
+        //order should be returned with an orderId.
+        order = orderService.getOrderById(orderId);
         return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-partner-by-id/{partnerId}")
     public ResponseEntity<DeliveryPartner> getPartnerById(@PathVariable String partnerId){
-        DeliveryPartner deliveryPartner = orderService.getPartnerById(partnerId);
+
+        DeliveryPartner deliveryPartner = null;
+
         //deliveryPartner should contain the value given by partnerId
+        deliveryPartner = orderService.getPartnerById(partnerId);
+
         return new ResponseEntity<>(deliveryPartner, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-order-count-by-partner-id/{partnerId}")
     public ResponseEntity<Integer> getOrderCountByPartnerId(@PathVariable String partnerId){
-        Integer orderCount = orderService.getOrderCountByPartnerId(partnerId);
+
+        Integer orderCount = 0;
+
         //orderCount should denote the orders given by a partner-id
+        orderCount = orderService.getOrderCountByPartnerId(partnerId);
+
         return new ResponseEntity<>(orderCount, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-orders-by-partner-id/{partnerId}")
     public ResponseEntity<List<String>> getOrdersByPartnerId(@PathVariable String partnerId){
-        List<String> orders = orderService.getOrdersByPartnerId(partnerId);
+        List<String> orders = null;
+
         //orders should contain a list of orders by PartnerId
+        orders = orderService.getOrdersByPartnerId(partnerId);
+
         return new ResponseEntity<>(orders, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-orders")
     public ResponseEntity<List<String>> getAllOrders(){
-        List<String> orders = orderService.getAllOrders();
+        List<String> orders = null;
+
         //Get all orders
+        orders = orderService.getAllOrders();
         return new ResponseEntity<>(orders, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-count-of-unassigned-orders")
     public ResponseEntity<Integer> getCountOfUnassignedOrders(){
-        Integer countOfOrders = orderService.getCountOfUnassignedOrders();
+        Integer countOfOrders = 0;
+
         //Count of orders that have not been assigned to any DeliveryPartner
+        countOfOrders = orderService.getCountOfUnassignedOrders();
+
         return new ResponseEntity<>(countOfOrders, HttpStatus.CREATED);
     }
 
-    @GetMapping("/get-count-of-orders-left-after-given-time/{partnerId}")
+    @GetMapping("/get-count-of-orders-left-after-given-time/{time}/{partnerId}")
     public ResponseEntity<Integer> getOrdersLeftAfterGivenTimeByPartnerId(@PathVariable String time, @PathVariable String partnerId){
 
         Integer countOfOrders = 0;
+
         //countOfOrders that are left after a particular time of a DeliveryPartner
+        countOfOrders = orderService.getOrdersLeftAfterGivenTimeByPartnerId(time,partnerId);
+
         return new ResponseEntity<>(countOfOrders, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-last-delivery-time/{partnerId}")
     public ResponseEntity<String> getLastDeliveryTimeByPartnerId(@PathVariable String partnerId){
-        String time = "null";
+        String time = null;
 
         //Return the time when that partnerId will deliver his last delivery order.
+        time = orderService.getLastDeliveryTimeByPartnerId(partnerId);
 
         return new ResponseEntity<>(time, HttpStatus.CREATED);
     }
@@ -107,7 +130,7 @@ public class OrderController {
 
         //Delete the partnerId
         //And push all his assigned orders to unassigned orders.
-
+        orderService.deletePartnerById(partnerId);
         return new ResponseEntity<>(partnerId + " removed successfully", HttpStatus.CREATED);
     }
 
@@ -116,7 +139,7 @@ public class OrderController {
 
         //Delete an order and also
         // remove it from the assigned order of that partnerId
-
+        orderService.deleteOrderById(orderId);
         return new ResponseEntity<>(orderId + " removed successfully", HttpStatus.CREATED);
     }
 }
--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -1,69 +1,139 @@
 package com.driver;
 
+import io.swagger.models.auth.In;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public class OrderRepository {
-    Map<String, Order> orderMap=new HashMap<>();
-    Map<String, DeliveryPartner> partnerMap=new HashMap<>();
-    Map<String,String> assignedOrder=new HashMap<>();
-    Map<DeliveryPartner,List<Order>> partnerOrderMap=new HashMap<>();
-    public void addOrder(Order order) {
-        orderMap.put(order.getId(), order);
+
+    HashMap<String,Order> orderHashMap = new HashMap<>();
+    HashMap<String,DeliveryPartner> deliveryPartnerHashMap = new HashMap<>();
+    HashMap<DeliveryPartner, List<Order>> orderPartnerPairHashMap = new HashMap<>();
+    HashSet<Order> assigned = new HashSet<>();
+    public void addOrder(Order order){
+        orderHashMap.put(order.getId(),order);
     }
-
-    public void addPartner(String partnerId) {
-        DeliveryPartner deliveryPartner=new DeliveryPartner(partnerId);
-        partnerMap.put(partnerId,deliveryPartner);
+    public void addDeliveryPartner(DeliveryPartner deliveryPartner){
+        deliveryPartnerHashMap.put(deliveryPartner.getId(),deliveryPartner);
     }
-
-    public void addOrderPartnerPair(String orderId, String partnerId) {
-        assignedOrder.put(orderId,partnerId);
-        Order order=getOrderById(orderId);
-        DeliveryPartner deliveryPartner=getPartnerById(partnerId);
-
-        if(partnerOrderMap.containsKey(deliveryPartner)){
-            List<Order> orderList=partnerOrderMap.get(deliveryPartner);
-            orderList.add(order);
-            partnerOrderMap.put(deliveryPartner,orderList);
-            return;
+    public void addOrderPartnerPair(String orderId,String partnerId){
+        if (deliveryPartnerHashMap.containsKey(partnerId) && orderHashMap.containsKey(orderId)){
+            List<Order> orders = new ArrayList<>();
+            if (orderPartnerPairHashMap.containsKey(deliveryPartnerHashMap.get(partnerId))){
+                orders = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId));
+            }
+            orders.add(orderHashMap.get(orderId));
+            orderPartnerPairHashMap.put(deliveryPartnerHashMap.get(partnerId),orders);
+            assigned.add(orderHashMap.get(orderId));
+            deliveryPartnerHashMap.get(partnerId).setNumberOfOrders(orders.size());
         }
-        List<Order> orderList=new ArrayList<>();
-        orderList.add(order);
-        partnerOrderMap.put(deliveryPartner,orderList);
     }
-
-    public DeliveryPartner getPartnerById(String partnerId) {
-        return partnerMap.get(partnerId);
+    public Order getOrderById(String orderId){
+        return orderHashMap.get(orderId);
     }
-
-    public Order getOrderById(String orderId) {
-        return orderMap.get(orderId);
+    public DeliveryPartner getPartnerById(String partnerId){
+        return deliveryPartnerHashMap.get(partnerId);
     }
-
-    public Integer getOrderCountByPartnerId(String partnerId) {
-        return partnerOrderMap.get(getPartnerById(partnerId)).size();
+    public Integer getOrderCountByPartnerId(String partnerId){
+        Integer count = 0;
+        if (orderPartnerPairHashMap.containsKey(deliveryPartnerHashMap.get(partnerId))){
+            count = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId)).size();
+        }
+        return count;
     }
-
-    public List<String> getOrdersByPartnerId(String partnerId) {
-      List<Order> orderList=partnerOrderMap.get(getPartnerById(partnerId));
-      List<String > ids=new ArrayList<>();
-      for(Order order:orderList){
-          ids.add(order.getId());
-      }
-        return ids;
+    public List<String> getOrdersByPartnerId(String partnerId){
+        List<String> orders = new ArrayList<>();
+        if (deliveryPartnerHashMap.containsKey(partnerId)) {
+            if (orderPartnerPairHashMap.containsKey(deliveryPartnerHashMap.get(partnerId))) {
+                List<Order> orderList = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId));
+                for (Order order : orderList){
+                    orders.add(order.getId());
+                }
+            }
+        }
+        return orders;
     }
-
-    public List<String> getAllOrders() {
-        return new ArrayList<>(orderMap.keySet());
+    public List<String> getAllOrders(){
+        List<String> orders = new ArrayList<>();
+        for (String orderId : orderHashMap.keySet()){
+            orders.add(orderId);
+        }
+        return orders;
     }
-
-    public Integer getCountOfUnassignedOrders() {
-        return orderMap.size()-assignedOrder.size();
+    public Integer getCountOfUnassignedOrders(){
+        return orderHashMap.size()-assigned.size();
+    }
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time,String partnerId){
+        Integer count = 0;
+        String[] str = time.split(":");
+        Integer currTime = Integer.parseInt(str[0])*60 + Integer.parseInt(str[1]);
+        if (orderPartnerPairHashMap.containsKey(deliveryPartnerHashMap.get(partnerId))){
+            List<Order> orders = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId));
+            for (Order order : orders){
+                if (orderHashMap.containsKey(order.getId())){
+                    Order currOrder = orderHashMap.get(order.getId());
+                    if (currTime < currOrder.getDeliveryTime()){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    public String getLastDeliveryTimeByPartnerId(String partnerId){
+        Integer time = 0;
+        List<Order> orders = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId));
+        for (Order order : orders){
+            time = Math.max(time,order.getDeliveryTime());
+        }
+        Integer minutes = time%60;
+        Integer hours = time/60;
+        String lastTime = "";
+        if (hours < 10){
+            lastTime = "0"+ hours;
+        }
+        else{
+            lastTime += hours;
+        }
+        lastTime += ":";
+        if (minutes == 0 || minutes < 10){
+            lastTime += "0" + minutes;
+        }
+        else{
+            lastTime += minutes;
+        }
+        return lastTime;
+    }
+    public void deletePartnerById(String partnerId){
+        if (orderPartnerPairHashMap.containsKey(deliveryPartnerHashMap.get(partnerId))){
+            List<Order> orders = orderPartnerPairHashMap.get(deliveryPartnerHashMap.get(partnerId));
+            for (Order order : orders){
+                assigned.remove(order);
+            }
+            orderPartnerPairHashMap.remove(deliveryPartnerHashMap.get(partnerId));
+            deliveryPartnerHashMap.remove(partnerId);
+        }
+    }
+    public void deleteOrderById(String orderId) {
+        if (orderHashMap.containsKey(orderId)){
+            if (assigned.contains(orderHashMap.get(orderId))){
+                assigned.remove(orderHashMap.get(orderId));
+            }
+            for (List<Order> orderList : orderPartnerPairHashMap.values()){
+                for (Order order : orderList){
+                    if (order.equals(orderHashMap.get(orderId))){
+                        orderList.remove(order);
+                        orderHashMap.remove(orderId);
+                        return;
+                    }
+                }
+            }
+            orderHashMap.remove(orderId);
+        }
     }
 }
